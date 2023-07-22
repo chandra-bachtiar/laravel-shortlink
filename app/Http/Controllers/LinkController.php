e5e0e1d17f42ac2c0a192c4296698a783dc4ce3e
chandra-bachtiar <chandrabahtiar25@gmail.com>
2023-07-22 11:26:32 +0700

    doc(link): menambahkan keterangan

--- app/Http/Controllers/LinkController.php
+++ app/Http/Controllers/LinkController.php
@@ -17,9 +17,16 @@ class LinkController extends Controller
      */
     public function index()
     {
+        /**
+         * $currentTime adalah variable yang akan menampung waktu sekarang
+         * $hour adalah variable yang akan menampung jam dari waktu sekarang
+         */
         $currentTime = Carbon::now();
-        $hour = $currentTime->hour+7;
+        $hour = $currentTime->hour + 7;
 
+        /**
+         * Melakukan pengecekan waktu sekarang
+         */
         if ($hour >= 0 && $hour < 3) {
             $greeting = 'Selamat Malam';
         } elseif ($hour >= 3 && $hour < 9) {
@@ -31,25 +38,62 @@ class LinkController extends Controller
         } else {
             $greeting = 'Selamat Malam';
         }
+
+        /**
+         * $agent adalah variable yang akan menampung data dari library agent yang berfungsi
+         * untuk mengecek apakah user menggunakan device mobile atau desktop
+         * 
+         * $isDesktop adalah variable yang akan menampung hasil dari pengecekan device
+         * $today adalah variable yang akan menampung waktu sekarang
+         * $dayName adalah variable yang akan menampung nama hari dari waktu sekarang
+         * $totalCreated adalah variable yang akan menampung total shortlink yang sudah dibuat
+         * $totalClicks adalah variable yang akan menampung total klik dari semua shortlink yang sudah dibuat
+         * $totalExpired adalah variable yang akan menampung total shortlink yang sudah expired
+         */
         $agent = new Agent();
         $isDesktop = $agent->isDesktop();
         $today = Carbon::now();
         $dayName = $today->translatedFormat('l');
-        $totalCreated = Shortlink::where('user_id', Auth::id())->where('deleted_at',null)->count();
-        $totalClicks = Shortlink::where('user_id', Auth::id())->where('deleted_at',null)->sum('clicks');
-        $totalExpired = Shortlink::where('user_id', Auth::id())->where('deleted_at',null)->where('expired', '<', Carbon::now())->count();
+        $totalCreated = Shortlink::where('user_id', Auth::id())->where('deleted_at', null)->count();
+        $totalClicks = Shortlink::where('user_id', Auth::id())->where('deleted_at', null)->sum('clicks');
+        $totalExpired = Shortlink::where('user_id', Auth::id())->where('deleted_at', null)->where('expired', '<', Carbon::now())->count();
 
-        return view('dashboard', compact('greeting', 'dayName' , 'totalCreated', 'totalClicks', 'totalExpired', 'isDesktop'));
+        /**
+         * menampilkan view dashboard dengan mengirimkan data greeting, dayName, totalCreated, totalClicks, totalExpired, isDesktop
+         */
+        return view('dashboard', compact('greeting', 'dayName', 'totalCreated', 'totalClicks', 'totalExpired', 'isDesktop'));
     }
 
     public function success(Request $request)
     {
+        /**
+         *  pengecekan apakah user sudah login atau belum
+         */
         if (Auth::check()) {
+
+            /**
+             * Jika sudah login maka akan menampilkan view success dengan mengirimkan data shorturl, diff, expired
+             * 
+             * $shorturl adalah variable yang akan menampung hasil generate atau link pendek yang telah jadi
+             * contohnya dari link panjang
+             * 
+             * $diff adalah variable yang akan menampung hasil perbedaan panjang dari url asli dengan url yang sudah dibuat pendek
+             * $expired adalah variable yang akan menampung hasil expired dari shortlink yang sudah dibuat
+             *
+             */
             $shorturl = $request->shorturl;
             $diff = $request->diff;
             $expired = $request->expired;
             return view('created', compact('shorturl', 'diff', 'expired'));
         } else {
+            /**
+             * Jika belum login maka akan menampilkan view success dengan mengirimkan data shorturl, diff
+             * 
+             * $shorturl adalah variable yang akan menampung hasil generate atau link pendek yang telah jadi
+             * contohnya dari link panjang
+             * 
+             * $diff adalah variable yang akan menampung hasil perbedaan panjang dari url asli dengan url yang sudah dibuat pendek
+             */
             $shorturl = $request->shorturl;
             $diff = $request->diff;
             return view('success', compact('shorturl', 'diff'));
@@ -58,18 +102,37 @@ class LinkController extends Controller
 
     public function createIndex()
     {
-        //expired is today + 7
+        /**
+         * Pada fungsi ini kita akan menampilkan view create dengan mengirimkan data expired dan url
+         * 
+         * $expired adalah variable yang akan menampung hasil expired dari shortlink yang sudah dibuat
+         * $url adalah variable yang akan menampung url dari website yang sedang diakses
+         */
         $expired = Carbon::now()->addDays(6)->format('Y-m-d');
         $url = url('/');
+
+        /**
+         * menampilkan view create dengan mengirimkan data expired dan url
+         */
         return view('create', compact('expired', 'url'));
     }
 
     public function mobileEdit($id)
     {
-        //expired is today + 7
+        /**
+         * Pada fungsi ini kita akan menampilkan view mobile-edit dengan mengirimkan data expired, url, dan link
+         * 
+         * $expired adalah variable yang akan menampung hasil expired dari shortlink yang sudah dibuat
+         * $url adalah variable yang akan menampung url dari website yang sedang diakses
+         * $link adalah variable yang akan menampung data dari shortlink yang akan diedit
+         */
         $expired = Carbon::now()->addDays(6)->format('Y-m-d');
         $url = url('/');
         $link = Shortlink::where('id', $id)->first();
+
+        /**
+         * menampilkan view mobile-edit dengan mengirimkan data expired, url, dan link
+         */
         return view('mobile-edit', compact('expired', 'url', 'link'));
     }
 
@@ -78,69 +141,95 @@ class LinkController extends Controller
      */
     public function create(Request $request)
     {
+        //fungsi untuk memvalidasi bahwa request dengan key long_url harus terisi jika tidak terisi maka error
         $this->validate($request, [
             'long_url' => 'required'
         ]);
 
+        //pengecekan untuk yang sudah login dan belum login
         if (Auth::check()) {
-            // dd(Auth::id());
+
+            //proses memasukan data ke database
+            /**
+             * shorturl = link pendek yang akan tergenerate
+             * longurl = link panjang yang akan digantikan oleh link pendek
+             * user_id = id dari user yang sudah login dan membuat shortlink
+             * expired = waktu expired dari shortlink tersebut
+             * 
+             * Str::random(6) = untuk membuat karakter random dengan panjang 6 karakter contohnya yusjsk
+             */
             $link = Shortlink::create([
+                /**
+                 * pada short url ada kondisi dimana jika request->custom_url itu tidak di isi maka akan tergenerate otomatis oleh fungsi str random 6
+                 * namun jika ada isinya maka akan mengambil dari custom url yang di input oleh user.
+                 */
                 'shorturl' => $request->custom_url != null ? $request->custom_url : Str::random(6),
                 'longurl' => $request->long_url,
+                /**
+                 * Auth::id() mengambil id dari service yang sudah disediakan oleh laravel dan juga brezee
+                 */
                 'user_id' => Auth::id(),
                 'expired' => $request->expired,
             ]);
 
+            /**
+             * $shorturl adalah variable yang akan menampung hasil generate atau link pendek yang telah jadi 
+             * contohnya dari link panjang
+             */
             $shorturl = url('/' . $link->shorturl);
+            // menghitng perbedaan panjang dari url asli dengan url yang sudah dibuat pendek
             $diff = strlen($request->long_url) - strlen($shorturl);
+            // mengambil expired dari database
             $expired = $link->expired;
+            // menampilkan pesan sukses
             toastr()->success('Link has been created successfully!');
+            // redirect ke halaman success
             return redirect()->route('url.success', compact('shorturl', 'diff', 'expired'));
         } else {
+            //proses memasukan data ke database
+            /**
+             * shorturl = link pendek yang akan tergenerate
+             * longurl = link panjang yang akan digantikan oleh link pendek
+             * expired = waktu expired dari shortlink tersebut
+             * 
+             * Str::random(6) = untuk membuat karakter random dengan panjang 6 karakter contohnya yusjsk
+             */
             $link = Shortlink::create([
                 'shorturl' => Str::random(6),
                 'longurl' => $request->long_url,
                 'expired' => Carbon::now()->addDays(1)->format('Y-m-d'),
             ]);
 
+            /**
+             * $shorturl adalah variable yang akan menampung hasil generate atau link pendek yang telah jadi 
+             * contohnya dari link panjang
+             */
             $shorturl = url('/' . $link->shorturl);
+            // menghitng perbedaan panjang dari url asli dengan url yang sudah dibuat pendek
             $diff = strlen($request->long_url) - strlen($shorturl);
+            // mengambil expired dari database
             $expired = $link->expired;
+            // menampilkan pesan sukses
             toastr()->success('Link has been created successfully!');
+            // redirect ke halaman success
             return redirect()->route('link.success', compact('shorturl', 'diff', 'expired'));
         }
     }
 
     public function redirect(string $shorturl)
     {
-        //get shortlink from database
+        //mengecek apakah shorturl ada di database
         $link = Shortlink::where('shorturl', $shorturl)->first();
 
-        //handle if link is unknown
+        //mengembalikan view 404 jika shorturl tidak ada di database
         if (!$link) {
             return view('404', ['message' => 'This link is unknown.']);
         }
 
-        //increment clicks
+        //menambahkan jumlah klik
         $link->increment('clicks');
-        //redirect to long url
+        //redirect ke longurl
         return redirect()->away($link->longurl);
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
     }
 
     /**
@@ -165,19 +254,18 @@ class LinkController extends Controller
      */
     public function delete(Request $request)
     {
-        // dd($request->all());
+        /**
+         * update data dengan mengisi deleted_at dengan waktu sekarang
+         */
         Shortlink::where('id', $request['id-delete'])->update([
             'deleted_at' => Carbon::now(),
         ]);
+
+        /**
+         * menampilkan pesan sukses
+         * redirect ke halaman dashboard
+         */
         toastr()->success('Link has been deleted!', 'Congrats', ['timeOut' => 2000]);
         return redirect()->route('dashboard');
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
     }
 }
